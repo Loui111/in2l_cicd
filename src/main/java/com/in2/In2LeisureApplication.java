70f3b1aaad32ab5b6b968795a69f6bdcb17e1299
genesisjk <genesisjk@gmail.com>
2023-02-15 16:56:54 +0900

    initial commit for environment

---
+++ src/main/java/com/in2/In2LeisureApplication.java
@@ -0,0 +1,13 @@
+package com.in2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In2LeisureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In2LeisureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in2/In2LeisureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In2LeisureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
